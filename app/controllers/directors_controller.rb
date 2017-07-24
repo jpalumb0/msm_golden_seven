c93dbbfda62bf39ab95dc31b8e9301c2f7983231
Joe <jpalumb0@chicagobooth.edu>
2017-07-24 02:41:34 +0000

    formatting and placing all details on index

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -33,31 +33,36 @@ class DirectorsController < ApplicationController
     
     def edit_form
         
-        @pic = Photo.find(params["some_id"])
+        @director = Director.find(params["some_id"])
         
         render("director_templates/edit_form.html.erb")
     end
     
     def update_row
         
-        @pic = Photo.find(params["the_id"])
+        @director = Director.find(params["the_id"])
         
         new_source = params["the_source"]
-        new_caption = params["the_caption"]
+        new_name = params["the_name"]
+        new_dob = params["the_dob"]
+        new_bio = params["the_bio"]
         
-        @pic.source = new_source
-        @pic.caption = new_caption
+        @director.image_url = new_source
+        @director.name = new_name
+        @director.dob = new_dob
+        @director.bio = new_bio
         
-        @pic.save
         
-        redirect_to("/photos/"+@pic.id.to_s)
+        @director.save
+        
+        redirect_to("/directors/"+@director.id.to_s)
     end
     
     def destroy_row
         
-        @pic = Photo.find(params["toast_id"])
-        @pic.destroy
+        @director = Director.find(params["toast_id"])
+        @director.destroy
         
-        redirect_to("/photos")
+        redirect_to("/directors")
     end
 end
